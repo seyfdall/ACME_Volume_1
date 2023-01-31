4b3c59b1f27316440b5f5adc5f1bec54059c9390
Dallin Seyfried <seyfdall@byu.edu>
2023-01-31 10:50:53 -0700

    Starting Newton's Method lab

--- NewtonsMethod/newtons_method.py
+++ NewtonsMethod/newtons_method.py
@@ -1,10 +1,12 @@
 # newtons_method.py
 """Volume 1: Newton's Method.
-<Name>
-<Class>
-<Date>
+<Name> Dallin Seyfried
+<Class> 001
+<Date> 1/31/2023
 """
-
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import newton as eye_of_newt
 
 # Problems 1, 3, and 5
 def newton(f, x0, Df, tol=1e-5, maxiter=15, alpha=1.):
@@ -24,7 +26,48 @@ def newton(f, x0, Df, tol=1e-5, maxiter=15, alpha=1.):
         (bool): Whether or not Newton's method converged.
         (int): The number of iterations computed.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    post = x0
+    converged = False
+    is_matrix = not np.isscalar(f(x0))
+
+    # If matrix solve using la.solve
+    if is_matrix:
+        i = 0
+        for _ in range(maxiter):
+            i += 1
+            pre = post
+            post = np.linalg.solve(Df(pre), f(pre))
+            if np.linalg.norm(post - pre) < tol:
+                converged = True
+                break
+    else:
+        # Cycle up to maxiter times and return if converged
+        i = 0
+        for _ in range(maxiter):
+            i += 1
+            pre = post
+            # Including alpha for backtracking
+            post = pre - alpha * f(pre) / Df(pre)
+            if np.linalg.norm(post - pre) < tol:
+                converged = True
+                break
+
+    return post, converged, i
+
+
+# Function to test newton() for problem 1
+def prob1_test():
+    f = lambda x: np.exp(x) - 2
+    Df = lambda x: np.exp(x)
+    print(f"Newton: {newton(f, 0.6, Df)}")
+    print(f"Scipy: {eye_of_newt(f, 0.6, Df)}")
+
+
+def prob3_test():
+    f = lambda x: np.sign(x) * np.power(np.abs(x), 1./3)
+    Df = lambda x: np.power(np.abs(x), -2./3) / 3.
+    print(newton(f,.01,Df,alpha=1))    # this should be (-327.679..., False, 15)
+    print(newton(f,.01,Df,alpha=.4))   # this should be (6.400...e-7, True, 6)
 
 
 # Problem 2
@@ -46,7 +89,22 @@ def prob2(N1, N2, P1, P2):
     Returns:
         (float): the value of r that satisfies the equation.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    # Initial Guess
+    r0 = 0.1
+    # Function f
+    f = lambda r: P1*((1 + r)**N1 - 1) - P2*(1 - (1 + r)**(-N2))
+    # Derivative of f
+    Df = lambda r: P1*N1*(1 + r)**(N1 - 1) - P2*N2*(1 + r)**(-N2 - 1)
+
+    # Compute the newton's method and return r
+    results = newton(f, r0, Df)
+    return results[0]
+
+
+# Function to test problem 2
+def prob2_test():
+    N1, N2, P1, P2 = 30, 20, 2000, 8000
+    print(prob2(N1, N2, P1, P2))
 
 
 # Problem 4
@@ -66,7 +124,29 @@ def optimal_alpha(f, x0, Df, tol=1e-5, maxiter=15):
         (float): a value for alpha that results in the lowest number of
             iterations.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    # Define linspace of alphas to try and iterations tested on
+    alphas = np.linspace(0.01, 1, 100)
+    iterations = [newton(f, x0, Df, tol, maxiter, alpha)[2] for alpha in alphas]
+
+    # Find the most effective alpha
+    min_iter = min(iterations)
+    min_index = iterations.index(min_iter)
+
+    # Plot the values
+    plt.plot(alphas, iterations)
+    plt.title("Optimal Alpha")
+    plt.ylabel("Iterations")
+    plt.xlabel("Alpha Value")
+    plt.tight_layout()
+    plt.show()
+
+    return alphas[min_index]
+
+
+def prob5_test():
+    f = lambda x: np.sign(x) * np.power(np.abs(x), 1./3)
+    Df = lambda x: np.power(np.abs(x), -2./3) / 3.
+    print(optimal_alpha(f,.01,Df)) # should return alpha closer to .3 than .4
 
 
 # Problem 6
@@ -80,7 +160,16 @@ def prob6():
     (0,1) or (0,−1) with alpha = 1, and to (3.75, .25) with alpha = 0.55.
     Return the intial point as a 1-D NumPy array with 2 entries.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    # Define search space
+    x_search = np.linspace(-0.25, 0, 100, endpoint=False)
+    y_search = np.linspace(0.00001, 0.25, 100)
+
+    # Define f and Df
+
+
+    # Search through with alpha = 1 looking for (0, 1) or (0, -1)
+
+    # Search through with alpha = 0.55 looking for (3.75, 0.25
 
 
 # Problem 7
