f4b353a2dacf4111bd17509d733fbf3290546920
Dallin Seyfried <seyfdall@byu.edu>
2023-02-14 10:38:42 -0700

    Initial Monte Carlo Integration commit

--- MonteCarloIntegration/montecarlo_integration.py
+++ MonteCarloIntegration/montecarlo_integration.py
@@ -1,10 +1,13 @@
 # montecarlo_integration.py
 """Volume 1: Monte Carlo Integration.
-<Name>
-<Class>
-<Date>
+<Name> Dallin Seyfried
+<Class> 001
+<Date> 2/14/2023
 """
 
+import numpy as np
+from scipy import linalg as la
+from scipy.stats.mvn import mvnun
 
 # Problem 1
 def ball_volume(n, N=10000):
@@ -18,7 +21,20 @@ def ball_volume(n, N=10000):
     Returns:
         (float): An estimate for the volume of the n-dimensional unit ball.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    # Get N points in n-D domains
+    points = np.random.uniform(-1, 1, (n, N))
+
+    # Determine how many points are within the circle
+    lengths = la.norm(points, axis=0, ord=2)
+    num_within = np.count_nonzero(lengths < 1)
+
+    # Estimate the Ball's area
+    return 2**n * (num_within / N)
+
+
+# Test ball volume - problem 1
+def test_ball_volume():
+    print(ball_volume(3))
 
 
 # Problem 2
@@ -35,11 +51,22 @@ def mc_integrate1d(f, a, b, N=10000):
         (float): An approximation of the integral of f over [a,b].
 
     Example:
-        >>> f = lambda x: x**2
-        >>> mc_integrate1d(f, -4, 2)    # Integrate from -4 to 2.
+        # >>> f = lambda x: x**2
+        # >>> mc_integrate1d(f, -4, 2)    # Integrate from -4 to 2.
         23.734810301138324              # The true value is 24.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    # Get N points in the interval [a,b]
+    points = np.random.uniform(a, b, N)
+
+    return (b - a) / N * np.sum(f(points))
+
+
+# Test mc_integrate - problem 2
+def test_mc_integrate1d():
+    f = lambda x: x**2
+    f_1 = lambda x: np.sin(x)
+    assert abs(24 - mc_integrate1d(f, -4, 2)) < 0.5, "Integrate for x**2 failed"
+    assert abs(0 - mc_integrate1d(f_1, -4, 2)) < 0.5, "Integrate for x**2 failed"
 
 
 # Problem 3
@@ -58,13 +85,42 @@ def mc_integrate(f, mins, maxs, N=10000):
 
     Example:
         # Define f(x,y) = 3x - 4y + y^2. Inputs are grouped into an array.
-        >>> f = lambda x: 3*x[0] - 4*x[1] + x[1]**2
+        # >>> f = lambda x: 3*x[0] - 4*x[1] + x[1]**2
 
         # Integrate over the box [1,3]x[-2,1].
-        >>> mc_integrate(f, [1, -2], [3, 1])
+        # >>> mc_integrate(f, [1, -2], [3, 1])
         53.562651072181225              # The true value is 54.
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    # Calculate the volume
+    maxs = np.array(maxs)
+    mins = np.array(mins)
+    volume = np.prod(maxs - mins)
+
+    # Get N points in n-D domains and scale them
+    points = np.random.uniform(0, 1, (len(maxs), N))
+
+    # Use array broadcasting to shift the points
+    for i in range(len(maxs)):
+        points[i] = points[i] * (maxs[i] - mins[i]) + mins[i]
+
+    return volume / N * np.sum(f(points))
+
+
+# Test mc_integrate - problem 3
+def test_mc_integrate():
+    # Test x^2 + y^2
+    maxs = [1, 1]
+    mins = [0, 0]
+    f = lambda x: x[0]**2 + x[1]**2
+    print(mc_integrate(f, mins, maxs))
+    assert abs(2/3 - mc_integrate(f, mins, maxs)) < 0.5, "Error on x**2 + y**2"
+
+    # Test x + y - wz^2
+    maxs = [-1, -2, -3, -4]
+    mins = [1, 2, 3, 4]
+    f_2 = lambda x: x[0] + x[1] - x[3] * x[2]**2
+    print(mc_integrate(f_2, mins, maxs, N=int(1e7)))
+    assert abs(0 - mc_integrate(f_2, mins, maxs, N=int(1e7))) < 0.5, "Error on x + y - wz**2"
 
 
 # Problem 4
@@ -79,4 +135,17 @@ def prob4():
     - Plot the relative error against the sample size N on a log-log scale.
         Also plot the line 1 / sqrt(N) for comparison.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    # Setup for the function
+    n = 4
+    mins = np.array([-3/2, 0, 0, 0])
+    maxs = np.array([3/4, 1, 1/2, 1])
+    f = lambda x: np.exp(-x.T @ x / 2) / ((2 * np.pi)**n/2)
+
+    # The distribution has mean 0 and covariance I (the nxn identity)
+    means, cov = np.zeros(4), np.eye(4)
+
+    # Compute the integral with SciPy
+    true_val = mvnun(mins, maxs, means, cov)[0]
+
+    
+
